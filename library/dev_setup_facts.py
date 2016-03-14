6f0cc3936f8d2186ca730b987b86b556cbbe01a1
nrser <neil@ztkae.com>
2016-03-14 15:39:55 -0700

    v0.1.0 - seems to be working for the wall setup

---
+++ library/dev_setup_facts.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import subprocess
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+        ),
+        supports_check_mode = False,
+    )
+
+    
+    facts = {}
+    
+    # get the project root via git (though we should be in dev/)
+    project_root = facts['dev_setup_project_root'] = subprocess.check_output(
+        ['git', 'rev-parse', '--show-toplevel']
+    ).rstrip()
+    
+    dev_dir = facts['dev_setup_dev_dir'] = os.path.join(project_root, 'dev')
+    
+    ruby_version_path = os.path.join(project_root, ".ruby-version")
+    if os.path.exists(ruby_version_path):
+        with open(ruby_version_path) as f:
+            facts["dev_setup_ruby_version"] = f.read().rstrip()
+    
+    
+    node_version_path = os.path.join(project_root, ".node-version")
+    if os.path.exists(node_version_path):
+        with open(node_version_path) as f:
+            facts["dev_setup_node_version"] = f.read().rstrip()
+    
+    gemfile_path = os.path.join(project_root, 'Gemfile')
+    if os.path.exists(gemfile_path):
+        facts["dev_setup_gemfile_exists"] = True
+    
+    module.exit_json(
+        changed = False,
+        ansible_facts = facts,
+    )
+
+# import module snippets
+from ansible.module_utils.basic import *
+from ansible.module_utils.known_hosts import *
+
+if __name__ == '__main__':
+    main()
